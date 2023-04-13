39631177eb690f93ae6edd45a5aa0b0a36aa086c
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-04-13 11:18:40 -0400

    Add files via upload

---
+++ assignment 11 q 1.c
@@ -0,0 +1,14 @@
+#set up dictionary with keys
+
+#suit;value
+cards_spade = {'spade':{'ace':{'spade':{'2':{'spade':{'3':{'spade':{'4':{'spade':{'5':{'spade':{'6':{'spade':{'7':{'spade':{'8':{'spade':{'9':{'spade':{'10':{'spade':{'jack':{'spade':{'queen':{'spade':{'king'}}}}}}}}}}}}}}}}}}}}}}}}}}
+cards_heart = {'heart':{'ace':{'heart':{'2':{'heart':{'3':{'heart':{'4':{'heart':{'5':{'heart':{'6':{'heart':{'7':{'heart':{'8':{'heart':{'9':{'heart':{'10':{'heart':{'jack':{'heart':{'queen':{'heart':{'king'}}}}}}}}}}}}}}}}}}}}}}}}}}
+cards_clover = {'clover':{'ace':{'clover':{'2':{'clover':{'3':{'clover':{'4':{'clover':{'5':{'clover':{'6':{'clover':{'7':{'clover':{'8':{'clover':{'9':{'clover':{'10':{'clover':{'jack':{'clover':{'queen':{'clover':{'king'}}}}}}}}}}}}}}}}}}}}}}}}}}
+cards_diamond = {'diamond':{'ace':{'diamond':{'2':{'diamond':{'3':{'diamond':{'4':{'diamond':{'5':{'diamond':{'6':{'diamond':{'7':{'diamond':{'8':{'diamond':{'9':{'diamond':{'10':{'diamond':{'jack':{'diamond':{'queen':{'diamond':{'king'}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+#value;suit
+cards_spade2 = {'ace':{'spade':{'2':{'spade':{'3':{'spade':{'4':{'spade':{'5':{'spade':{'6':{'spade':{'7':{'spade':{'8':{'spade':{'9':{'spade':{'10':{'spade':{'jack':{'spade':{'queen':{'spade':{'king':{'spade'}}}}}}}}}}}}}}}}}}}}}}}}}}
+cards_heart2 = {'ace':{'heart':{'2':{'heart':{'3':{'heart':{'4':{'heart':{'5':{'heart':{'6':{'heart':{'7':{'heart':{'8':{'heart':{'9':{'heart':{'10':{'heart':{'jack':{'heart':{'queen':{'heart':{'king':{'spade'}}}}}}}}}}}}}}}}}}}}}}}}}}
+cards_clover2 ={'ace':{'clover':{'2':{'clover':{'3':{'clover':{'4':{'clover':{'5':{'clover':{'6':{'clover':{'7':{'clover':{'8':{'clover':{'9':{'clover':{'10':{'clover':{'jack':{'clover':{'queen':{'clover':{'king':{'clover'}}}}}}}}}}}}}}}}}}}}}}}}}}
+cards_diamond2 = {'ace':{'diamond':{'2':{'diamond':{'3':{'diamond':{'4':{'diamond':{'5':{'diamond':{'6':{'diamond':{'7':{'diamond':{'8':{'diamond':{'9':{'diamond':{'10':{'diamond':{'jack':{'diamond':{'queen':{'diamond':{'king':{'diamond'}}}}}}}}}}}}}}}}}}}}}}}}}}
+
